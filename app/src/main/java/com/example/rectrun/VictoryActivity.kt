cd97f5013dcc7b91408b4cde8504e4f34a903963
KimikoXmaka <113034039+KimikoXmaka@users.noreply.github.com>
2023-05-19 07:15:31 -0500

    finished

--- app/src/main/java/com/example/rectrun/VictoryActivity.kt
+++ app/src/main/java/com/example/rectrun/VictoryActivity.kt
@@ -1,11 +1,18 @@
 package com.example.rectrun
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class VictoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_victory)
+    }
+
+    fun fullrestart(view: View) {
+        val fullrestart = Intent(this, MainActivity::class.java)
+        startActivity(fullrestart)
     }
 }
